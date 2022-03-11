03f05fbd89c8c156e86beb82eb574a2543ec8f84
Joey Wilhelm <tarkatronic@gmail.com>
2022-03-11 11:55:29 -0700

    Add a simple benchmarking script

---
+++ benchmark.py
@@ -0,0 +1,28 @@
+import gc
+import timeit
+
+from asherah import Asherah, AsherahConfig
+
+config = AsherahConfig(
+    kms_type="static",
+    metastore="memory",
+    service_name="TestService",
+    product_id="TestProduct",
+    session_cache=True,
+)
+crypt = Asherah()
+crypt.setup(config)
+
+data = b"mysecretdata"
+
+crypt_cycle = """
+encrypted = crypt.encrypt("partition", data)
+decrypted = crypt.decrypt("partition", encrypted)
+"""
+
+print(f'Benchmarking encrypt/decrypt round trips of "{data}".')
+for loop_size in [100, 1000, 10000, 100000]:
+    result = timeit.timeit(
+        stmt=crypt_cycle, setup="gc.enable()", number=loop_size, globals=globals()
+    )
+    print(f"Executed {loop_size} iterations in {result} seconds.")
