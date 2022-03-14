39a70b9822d892dd9785889ce627786f88a67b65
Joey Wilhelm <tarkatronic@gmail.com>
2022-03-14 12:39:22 -0600

    Add initial tests

--- tests/test_asherah.py
+++ tests/test_asherah.py
@@ -2,7 +2,34 @@
 
 from unittest import TestCase
 
+from asherah import Asherah, AsherahConfig
+
 
 class AsherahTest(TestCase):
-    def test_fake(self):
-        self.assertEqual(True, True)
+    def setUp(self) -> None:
+        self.config = AsherahConfig(
+            kms="static",
+            metastore="memory",
+            service_name="TestService",
+            product_id="TestProduct",
+            verbose=True,
+            enable_session_caching=True,
+        )
+        self.asherah = Asherah()
+        self.asherah.setup(self.config)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.asherah.shutdown()
+        return super().tearDown()
+
+    def test_input_string_is_not_in_encrypted_data(self):
+        data = "mysecretdata"
+        encrypted = self.asherah.encrypt("partition", data)
+        self.assertFalse(data in encrypted)
+
+    def test_decrypted_data_equals_original_data(self):
+        data = b"mysecretdata"
+        encrypted = self.asherah.encrypt("partition", data)
+        decrypted = self.asherah.decrypt("partition", encrypted)
+        self.assertEqual(decrypted, data)
